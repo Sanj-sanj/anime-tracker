292c94c1c764ef839bc40c232e455e6ab3bcf771
Sanjeet Sidhu <sanjeetu3@gmail.com>
2020-10-07 23:25:18 -0700

    fixed dependencies

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const server = express()
 const port = process.env.PORT || 3001;
-const scraper = require('./scraper.js')
 const graphQL = require('./graphql.js');
 const thisQuery = require('./query.js')
 
